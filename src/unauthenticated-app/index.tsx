257a8acf7b103bfcf872366918c33e3ea992d1ad
boundlesssea <xuzefeng159@163.com>
2024-08-21 16:33:51 +0800

    add image for login page

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -2,7 +2,9 @@ import React, {useState} from "react";
 import {useAuth} from "../context/auth-context";
 import {Button, Card, Divider, Form, Input} from "antd";
 import styled from "@emotion/styled";
-
+import head from 'assets/head.png';
+import left from 'assets/left.jpg';
+import right from 'assets/right.jpg';
 
 export const UnauthenticatedApp = () => {
     const [isRegister, setIsRegister] = useState(false)
@@ -14,6 +16,8 @@ export const UnauthenticatedApp = () => {
 
     return (
         <Container>
+            <Head src={head}/>
+            <Background/>
             <ShadowCard>
                 <Title>{isRegister ? "请注册" : "请登录"}</Title>
                 <Form onFinish={handleSubmit}>
@@ -36,6 +40,20 @@ export const UnauthenticatedApp = () => {
     )
 }
 
+const Head = styled.img`
+  width: 5%;
+`
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: left bottom, right bottom;
+  background-image: url(${left}), url(${right});
+  background-size: 50% 80%,50% 80%;
+`
 
 const Container = styled.div`
   display: flex;
@@ -48,7 +66,7 @@ const ShadowCard = styled(Card)`
   width: 40rem;
   min-height: 56rem;
   padding: 3.2rem 4rem;
-  border-radius: 0.3rem;
+  border-radius: 0.5rem;
   box-sizing: border-box;
   box-shadow: rgba(0, 0, 0, 0.1) 0 0 10px;
   text-align: center;
